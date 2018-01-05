9f32c650832be5b19482fe28216d62f6e7790145
SNEHA <SNEHAK2110ROX@GMAIL.COM>
2018-01-05 21:45:01 +0530

    top players page added

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,8 @@ class Header extends Component{
     constructor(props){
         super(props);
         this.state = {
-            search_place_holder: 'Player Tag #XXXXXX'
+            search_place_holder: 'Player Tag #XXXXXX',
+            dropdown_title: 'Player'
         }
     }
 
@@ -22,13 +23,13 @@ class Header extends Component{
     searchDropDownClicked(id){
         switch(id){
             case 1:
-                this.setState({search_place_holder: 'Player Tag #XXXXXX'});
+                this.setState({search_place_holder: 'Player Tag #XXXXXX' , dropdown_title: 'Player'});
                 break;
             case 2:
-                this.setState({search_place_holder: 'Clan Tag #XXXXXX'});
+                this.setState({search_place_holder: 'Clan Tag #XXXXXX' ,  dropdown_title: 'Clan'});
                 break;
             case 3:
-                this.setState({search_place_holder: 'Tournament Tag #XXXXXX'});
+                this.setState({search_place_holder: 'Tournament Tag #XXXXXX' ,  dropdown_title: 'Tournament'});
                 break;
         }
     }
@@ -36,49 +37,47 @@ class Header extends Component{
     render (){
         return(
         <div>
-	<Navbar inverse collapseOnSelect className="custom-nav">
+	<Navbar inverse collapseOnSelect className="custom-nav" sticky>
 		<Navbar.Header>
 			<Navbar.Brand>
-				<div>Clash Tracker</div>
+				<Link to="/">Clash Tracker</Link>
 			</Navbar.Brand>
 			
             <Navbar.Toggle />
 		</Navbar.Header>
             <Nav>
                 <Navbar.Form>
-        <form> 
-            <FormGroup className = "custom-header-form" >
-                <InputGroup>
-                <DropdownButton
+                <form> 
+                    <FormGroup className = "custom-header-form" >
+                        <InputGroup>
+                    <DropdownButton
 					componentClass={InputGroup.Button}
 					id="input-dropdown-addon"
-					title="Player"
-				>
-				<MenuItem key="1" onClick={()=>this.searchDropDownClicked(1)}>Player</MenuItem>
-                <MenuItem key="2" onClick={()=>this.searchDropDownClicked(2)}>Clan</MenuItem>
-                <MenuItem key="3" onClick={()=>this.searchDropDownClicked(3)}>Tournament</MenuItem>
-				</DropdownButton>
-				<FormControl type="text" placeholder={this.state.search_place_holder}/>
-				<InputGroup.Button>
-					<Button><Glyphicon glyph="search" /></Button>
-				</InputGroup.Button>
-			</InputGroup>
-            </FormGroup>
-        </form>
+					title={this.state.dropdown_title}
+				    >
+				        <MenuItem key="1" onClick={()=>this.searchDropDownClicked(1)}>Player</MenuItem>
+                        <MenuItem key="2" onClick={()=>this.searchDropDownClicked(2)}>Clan</MenuItem>
+                        <MenuItem key="3" onClick={()=>this.searchDropDownClicked(3)}>Tournament</MenuItem>
+				    </DropdownButton>
+				    <FormControl type="text" placeholder={this.state.search_place_holder}/>
+				        <InputGroup.Button>
+					        <Button><Glyphicon glyph="search" /></Button>
+				        </InputGroup.Button>
+			        </InputGroup>
+                    </FormGroup>
+                </form>
         </Navbar.Form>
             </Nav>
             
 		<Navbar.Collapse>
 			<Nav pullRight>
 				<NavItem>
-                  <Link to = "/topplayers">
-				   Top Players
-                  </Link>
+                    <Link to = "/topplayers" className="nav-a">
+				        Top Players
+                    </Link>
 				</NavItem>
 				<NavItem>
-                    <Link to = "/topplayers">
 					Top Clans
-                    </Link>
 				</NavItem>
 			</Nav>
 			
--- src/components/TopPlayers.js
+++ src/components/TopPlayers.js
@@ -1,7 +1,7 @@
 import React , {Component} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import axios from 'axios';
+import {Table} from 'react-bootstrap';
 
 import {getTopPlayers} from '../actions';
 
@@ -15,15 +15,43 @@ class TopPlayers extends Component{
 
      return_top_player_list = ({top_player_reducer})=>{
         if(top_player_reducer.top_players){
+            var i = 0;
         return top_player_reducer.top_players.map((Player)=>{
+                if(Player.clan){
+                    var clanName = Player.clan.name;
+                    var badge_img_src = Player.clan.badge.image;
+                }else{
+                    var clanName = "N/A"
+                    var badge_img_src ="";
+                }
                 if(Player){
+                    i++;
                     return(
-                        <div key={Player.tag}>
-                            {Player.name}
-                        </div>
+                        <tr key = {Player.tag}>
+                            <td>
+                                {i}
+                            </td>
+                            <td>
+                                #{Player.tag}
+                            </td>
+                            <td>
+                                {Player.name}
+                            </td>
+                            <td>
+                                {Player.trophies}
+                            </td>
+                            <td>
+                                
+                               <img height={28} width={25} alt="" src = {badge_img_src}/> {clanName}
+                            </td>
+                            <td>
+                                {Player.previousRank}
+                            </td>
+                        </tr>
                     );
                 }
             });
+            return top_player_reducer.top_players;
         }
     }
 
@@ -52,10 +80,33 @@ class TopPlayers extends Component{
         
 
     }
+
+     imageFormatter = (cell, row)=>{
+        return "<img src='"+cell+"'/>" ;
+      }
+
     render(){
         return(
             <div>
-                {this.return_top_player_list(this.props)}
+                {/* <BootstrapTable hover={true} data={this.return_top_player_list(this.props)} pagination>
+                   <TableHeaderColumn dataField = "tag" isKey="true" >Player Tag</TableHeaderColumn>
+                   <TableHeaderColumn dataField = "name">Name</TableHeaderColumn>
+                   <TableHeaderColumn dataField = "trophies">Trophies</TableHeaderColumn>
+                   <TableHeaderColumn dataField = "clan.name">Clan</TableHeaderColumn>
+                </BootstrapTable> */}
+                <Table responsive>
+                    <thead>
+			            <tr>
+                            <th>Rank</th>
+                            <th>Tag</th>
+                            <th>Name</th>
+                            <th>Trophies</th>
+                            <th>Clan</th>
+                            <th>Previous Rank</th>
+			            </tr>
+                        {this.return_top_player_list(this.props)}
+		            </thead>
+                </Table>
             </div>
         );
     }
