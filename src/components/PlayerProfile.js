ddda50977ed1488b96df07266bd9cba266b97a90
Avin Vij <avinvij26@gmail.com>
2018-01-07 11:24:20 -0800

    player profile page started designing

--- src/components/PlayerProfile.js
+++ src/components/PlayerProfile.js
@@ -1,9 +1,12 @@
 import React , {Component} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import {ProgressBar ,Image ,Panel , Grid, Row, Tabs , Tab , Col} from 'react-bootstrap';
 
 //action
 import {getPlayerProfile} from '../actions'
+
+const player_level_img_link = require('../images/level_star.png');
 class PlayerProfile extends Component{
 
 
@@ -21,7 +24,7 @@ class PlayerProfile extends Component{
             console.log(player_reducer.player_profile.name);
             return (
                 <div>
-                    {player_reducer.player_profile.name}
+                    
                 </div>
             )
         }
@@ -38,7 +41,36 @@ class PlayerProfile extends Component{
     render(){
         return(
             <div>
-                Player Profile {this.displayPlayerProfile(this.props)}
+                {!this.props.player_reducer.player_profile && <ProgressBar bsStyle="success"  active now={100} label={"Loading"} />}
+                {this.props.player_reducer.player_profile && 
+                <Panel className="player-panel">
+                    <Grid fluid>
+                    <Row>
+                        <Col sm = {1} md={1} lg={1} className = "custom-col"><b className = "player-name">{this.props.player_reducer.player_profile.name}</b></Col>
+                        <Col sm={1} md={1} lg={1}><div><img width = {42} height = {42} src = {player_level_img_link} tabIndex = "-1" title = {this.props.player_reducer.player_profile.stats.level}/><b className="player-level-tag">{this.props.player_reducer.player_profile.stats.level} </b></div></Col>
+                    </Row>
+                    <Row><h5><img width = {30} height={35} src = {this.props.player_reducer.player_profile.clan.badge.image}/>  {this.props.player_reducer.player_profile.clan.name} ></h5><br/>
+                    </Row>
+                    </Grid>
+                    <Tabs defaultActiveKey={1} id="uncontrolled-tab-example">
+                        <Tab eventKey={1} title="Profile">
+                            <div>
+                                <h4>Statistics</h4>
+
+                            </div>
+                        </Tab>
+                        <Tab eventKey={2} title="Battles">
+                            Tab 2 content
+                        </Tab>
+                        <Tab eventKey={3} title="Cards">
+                            Tab 3 content
+                        </Tab>
+                        <Tab eventKey={4} title="Decks">
+                            Tab 4 content
+                        </Tab>
+                </Tabs>
+                </Panel>
+                }
             </div>
         )
     }
