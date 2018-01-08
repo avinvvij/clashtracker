3af444341f1e7aa5d16736a38788685bb6dbe499
Avin Vij <vij_a@India.XoriantCorp.com>
2018-01-08 15:42:57 +0530

    player profile page with basic data

--- src/components/PlayerProfile.js
+++ src/components/PlayerProfile.js
@@ -61,10 +61,8 @@ class PlayerProfile extends Component{
                             <div>
                             <Row>
                             <Col lg = {12} md={12} sm = {12} className="player-custom-col">
-                                <h4>Player Statistics...</h4>
+                                <h2>Player Statistics...</h2>
                             </Col>
-                            </Row>
-                            <Row>
                             </Row>
                             <Row>
                                 <Col lg = {3} md={6} sm = {6} xs = {6} className="player-custom-col">
@@ -79,11 +77,51 @@ class PlayerProfile extends Component{
                                  <Col lg = {3} md={6} sm = {6} xs = {6} className="player-custom-col">
                                     <img width={18} height={18} src={require('../images/cards_found_ico.png')} /><b> Favorite Card:  {this.props.player_reducer.player_profile.stats.favoriteCard.name} <img src={this.props.player_reducer.player_profile.stats.favoriteCard.icon} width={30} height={33}/></b>
                                 </Col>
-                                                                <Col lg = {3} md={6} sm = {6} xs = {6} className="player-custom-col">
-                                    <img width={18} height={18} src={require('../images/cards_found_ico.png')} /><b> Total Donation:  {this.props.player_reducer.player_profile.stats.totalDonations}</b>
+                                <Col lg = {3} md={6} sm = {6} xs = {6} className="player-custom-col">
+                                    <img width={18} height={18} src={require('../images/total_donations_icon.png')} /><b> Total Donations:  {this.props.player_reducer.player_profile.stats.totalDonations}</b>
                                 </Col>
                                  <Col lg = {3} md={6} sm = {6} xs = {6} className="player-custom-col">
-                                    <img width={18} height={18} src={require('../images/cards_found_ico.png')} /><b> Tournament Cards Won:  {this.props.player_reducer.player_profile.stats.tournamentCardsWon}</b>
+                                    <img width={18} height={18} src={require('../images/tournament_cards_won_ico.png')} /><b> Tournament Cards Won:  {this.props.player_reducer.player_profile.stats.tournamentCardsWon}</b>
+                                </Col>
+                                 <Col lg = {3} md={6} sm = {6} xs = {6} className="player-custom-col">
+                                    <img width={18} height={18} src={require('../images/crown_icon.png')} /><b> Three Crown Wins:  {this.props.player_reducer.player_profile.stats.threeCrownWins}</b>
+                                </Col>
+                                <Col lg = {3} md={6} sm = {6} xs = {6} className="player-custom-col">
+                                    <img width={18} height={18} src={require('../images/cr_arena_icon.png')} /><b> Arena:  {this.props.player_reducer.player_profile.arena.name} <img src={"http://www.clashapi.xyz/images/leagues/"+this.props.player_reducer.player_profile.arena.name.toLowerCase().replace(' ','-')+".png"} height = {20} width = {18} /></b>
+                                </Col>
+                            <hr />
+                            </Row>
+                            
+                            {/* Game Statistics */}
+                            <Row>
+                            <Col lg = {12} md={12} sm = {12} className="player-custom-col">
+                                <h2>Game Statistics...</h2>
+                            </Col>
+                            </Row>
+                            <Row>
+                                <Col lg = {3} md={6} sm = {6} xs = {6} className="player-custom-col">
+                                    <img width={18} height={18} src={require('../images/trophy_icon.png')} /><b> Total Games Played:  {this.props.player_reducer.player_profile.games.total}</b>
+                                </Col>
+                                 <Col lg = {3} md={6} sm = {6} xs = {6} className="player-custom-col">
+                                    <img width={18} height={18} src={require('../images/trophy_icon.png')} /><b> Total Wins:  {this.props.player_reducer.player_profile.games.wins}</b>
+                                </Col>
+                                <Col lg = {3} md={6} sm = {6} xs = {6} className="player-custom-col">
+                                    <img width={18} height={18} src={require('../images/cards_found_ico.png')} /><b> Tournament Games:  {this.props.player_reducer.player_profile.games.tournamentGames}</b>
+                                </Col>
+                                 <Col lg = {3} md={6} sm = {6} xs = {6} className="player-custom-col">
+                                    <img width={18} height={18} src={require('../images/cards_found_ico.png')} /><b> Tournament Cards Won:  {this.props.player_reducer.player_profile.stats.tournamentCardsWon} </b>
+                                </Col>
+                                <Col lg = {3} md={6} sm = {6} xs = {6} className="player-custom-col">
+                                    <img width={18} height={18} src={require('../images/total_donations_icon.png')} /><b> Challenge Cards Won:  {this.props.player_reducer.player_profile.stats.challengeCardsWon}</b>
+                                </Col>
+                                 <Col lg = {3} md={6} sm = {6} xs = {6} className="player-custom-col">
+                                    <img width={18} height={18} src={require('../images/tournament_cards_won_ico.png')} /><b> Challenge Max Wins:  {this.props.player_reducer.player_profile.stats.challengeMaxWins}</b>
+                                </Col>
+                                 <Col lg = {3} md={6} sm = {6} xs = {6} className="player-custom-col">
+                                    <img width={18} height={18} src={require('../images/crown_icon.png')} /><b> Total Draws:  {this.props.player_reducer.player_profile.games.draws}</b>
+                                </Col>
+                                <Col lg = {3} md={6} sm = {6} xs = {6} className="player-custom-col">
+                                    <img width={18} height={18} src={require('../images/cr_arena_icon.png')} /><b> Total Losses:  {this.props.player_reducer.player_profile.games.losses} <img src={"http://www.clashapi.xyz/images/leagues/"+this.props.player_reducer.player_profile.arena.name.toLowerCase().replace(' ','-')+".png"} height = {20} width = {18} /></b>
                                 </Col>
                             </Row>
                             </div>
